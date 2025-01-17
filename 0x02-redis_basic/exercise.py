670da811be00eb9427e4c048b5f4dff2fad4223e
craizytech <craizytech@gmail.com>
2025-01-17 16:20:54 +0300

    from protected to private instance variable

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,8 +9,8 @@ class Cache:
     """This class contains operations done on redis"""
     def __init__(self):
         """This is the constructor method of the class"""
-        _redis = redis.Redis()
-        _redis.flushdb()
+        __redis = redis.Redis()
+        __redis.flushdb()
 
     def store(self, data: any):
         """
