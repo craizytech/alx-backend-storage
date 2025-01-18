df85492ad94de9431bca7502c39d1e7c5d176722
craizytech <craizytech@gmail.com>
2025-01-18 09:42:40 +0300

    reimplemented the web cache tracker to adhere to the task description that we are tracking and caching the number of times the url has been accessed

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """This Module implements an expiring web cache and tracker"""
 
 import redis
@@ -9,35 +9,17 @@ import requests
 r = redis.Redis(decode_responses=True)
 
 
-def load_from_cache(func: Callable) -> Callable:
-    """
-    retrieves the web page from the cache if it does not exit at
-    the cache then it gets it from the web page itself
-    """
-    @wraps(func)
-    def wrapper_function(url: str) -> str:
-        cached_webpage = r.get(url)
-        if cached_webpage:
-            return cached_webpage
-
-        web_content = func(url)
-        r.setex(url, 10, web_content)
-        return web_content
-
-    return wrapper_function
-
-
 def call_counts(func: Callable) -> Callable:
     """tracks how many times a url has been accessed"""
     @wraps(func)
     def wrapper_function(url: str) -> str:
         r.incr(f"count:{url}")
+        r.setex(f"count:{url}", 10)
         return func(url)
 
     return wrapper_function
 
 
-@load_from_cache
 @call_counts
 def get_page(url: str) -> str:
     """Fetch the url content from the web"""
